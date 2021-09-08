cb6f249f7ff0697357b7adfd869c020b86200b4d
Matt <Mattxiong_14@hotmail.com>
2021-09-08 09:57:13 -0500

    phase1 completed

---
+++ Bank.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace HeistPartII
+{
+
+    public class Bank
+    {
+        public int AlarmScore { get; set; }
+        public int CashOnHand { get; set; }
+        public int VaultScore { get; set; }
+        public int SecurityGuardScore { get; set; }
+
+        public bool isSecure
+        {
+            get
+            {
+                if( AlarmScore <= 0 && VaultScore <= 0 && SecurityGuardScore <= 0 )
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+
+    }
+}
